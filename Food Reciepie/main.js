90b47324054bd2e6fe54516c3eecdde4b84591a1
Karthikeyan Govindaraj <30545166+gkarthiks@users.noreply.github.com>
2018-01-22 22:06:35 -0800

    WIP

---
+++ Food Reciepie/main.js
@@ -0,0 +1,21 @@
+﻿/**
+ * Listens for the app launching then creates the window
+ *
+ */
+chrome.app.runtime.onLaunched.addListener(function() {
+  // Center window on screen.
+  var screenWidth = screen.availWidth;
+  var screenHeight = screen.availHeight;
+  var width = 500;
+  var height = 300;
+
+  chrome.app.window.create('index.html', {
+    id: "helloWorldID",
+    outerBounds: {
+      width: width,
+      height: height,
+      left: Math.round((screenWidth-width)/2),
+      top: Math.round((screenHeight-height)/2)
+    }
+  });
+});
